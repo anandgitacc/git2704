55aefcd5e68dc96597ee887a363bfb88cfb5182a
anandgitacc <anand.akmail@gmail.com>
2022-04-27 15:18:30 +0530

    firstgit

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,23 @@
+package org.employee;
+
+public class EmployeeDetails {
+	
+private void employeeId() {
+		
+		System.out.println("Employee id is : 123");
+	}
+	
+	private void employeeMobile() {
+		
+		System.out.println("Employee modile:8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		EmployeeDetails e = new EmployeeDetails();
+		
+		e.employeeId();
+		e.employeeMobile();
+	}
+
+}
